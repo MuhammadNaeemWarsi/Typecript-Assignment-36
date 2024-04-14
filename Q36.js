0c92e96987dfabc002d1d807b19697088ff014ca
Muhammad Naeem Warsi <161728783+MuhammadNaeemWarsi@users.noreply.github.com>
2024-04-14 23:04:39 +0500

    Q.36 complete

---
+++ Q36.js
@@ -0,0 +1,10 @@
+//T-Shirt: Write a function called make_shirt() that accepts a size and the
+//text of a message that should be printed on the shirt. The function should print
+//a sentence summarizing the size of the shirt and the message printed on it.
+//Call the function.
+function make_shirt(size, message) {
+    console.log("i am making a ".concat(size, " T.shirt with message of ").concat(message));
+}
+make_shirt("small", "I love Pakistan");
+make_shirt("Medium", "I love Pakistan");
+make_shirt("Large", "I love Pakistan");
